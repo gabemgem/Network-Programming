2ac07caea54ea19293ddfab73f7b7ed392e616c7
gmaayan <gabemgem@gmail.com>
2018-09-23 16:15:55 -0400

    Added packet handler to deal with incoming and outgoing packets

---
+++ HW01/packet_handler.c
@@ -0,0 +1,123 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "packet_handler.h"
+
+packet p;
+char* out_packet[PACKETSIZE];
+int out_packet_length=0;
+twobyte block_num = 0;
+
+
+
+
+void parse_packet(packet* p, const char* pbuffer) {
+	char c_op[2];
+	memcpy(c_op, pbuffer, sizeof(twobyte));
+	p.op = atoi(c_op);
+
+	if(IS_RRQ(p.op) || IS_WRQ(p.op)) {
+		int filenamel = 0;
+		int model = 0;
+
+		memset(p.filename, 0, sizeof(p.filename));
+		memset(p.mode, 0, sizeof(p.mode));
+
+		while(*(pbuffer+sizeof(twobyte)+filenamel)!='0') {
+			(p.filename)[filenamel] = pbuffer[sizeof(twobyte)+filenamel];
+			filenamel++;
+		}
+		(p.filename)[filenamel] = ' ';
+
+		while(*(pbuffer+sizeof(twobyte)+filenamel+model+1)!='0') {
+			(p.mode)[model] = pbuffer[sizeof(twobyte)+filenamel+model+1];
+			model++;
+		}
+	}
+
+	else if(IS_DAT(p.op)) {
+		char c_blnum[2];
+		memcpy(c_blnum, pbuffer+sizeof(twobyte), sizeof(twobyte));
+		p.blnum = atoi(c_blnum);
+
+		p.data_l=0;
+		memset(p.data, 0, sizeof(p.data));
+		while(*(pbuffer+(2*sizeof(twobyte))+p.data_l)!=NULL) {
+			(p.data)[p.data_l] = pbuffer[(2*sizeof(twobyte))+p.data_l];
+			p.data_l++;
+		}
+	}
+
+	else if(IS_ACK(p.op)) {
+		char c_blnum[2];
+		memcpy(c_blnum, pbuffer+sizeof(twobyte), sizeof(twobyte));
+		p.blnum = atoi(c_blnum);
+	}
+
+	else if(IS_ERR(p.op)) {
+		char c_ecode[2];
+		memcpy(c_ecode, pbuffer+sizeof(twobyte), sizeof(twobyte));
+		p.errcode = atoi(c_ecode);
+
+		memset(p.errmes, 0, sizeof(p.errmes));
+		p.errmes_l=0;
+
+		while(*(pbuffer+(2*sizeof(twobyte))+p.errmes_l)!='0') {
+			(p.errmes)[p.errmes_l] = pbuffer[(2*sizeof(twobyte))+p.errmes_l];
+			p.errmes_l++;
+		}
+		(p.errmes)[p.errmes_l] = ' ';
+	}
+
+	else{
+		perror("Improper packet received.\n");
+	}
+}
+
+void packet_handler(packet* p, const char* pbuffer, int* state) {
+	parse_packet(p, pbuffer);	
+
+	if(*state==IS_STARTING) {
+		handle_request(p, pbuffer);
+		if(IS_RRQ(p.op)) {
+			*state = IS_WRITING;
+		}
+		else if(IS_WRQ(p.op)) {
+			*state = IS_READING;
+		}
+		else {
+			send_error(ECODE_4);
+		}
+	}
+
+
+	else if(*state==IS_WRITING) {
+		if(IS_ACK(p.op)) {
+			send_data();
+		}
+		else {
+			handle_error();
+		}
+	}
+	else if(*state==IS_READING) {
+		if(IS_DAT(p.op)) {
+			send_ack();
+		}
+		else {
+			handle_error();
+		}
+	}
+	
+}
+
+void make_packet(int type, char* data, int data_l) {
+	out_packet = realloc(out_packet, out_packet_length+data_l);
+	memmove(out_packet+out_packet_length, data, data_l);
+	out_packet_length+=data_l;
+}
+
+void send_data(packet* p, char* pbuffer) {
+	twobyte op = htons(3);
+
+}
+
---
+++ HW01/packet_handler.h
@@ -0,0 +1,57 @@
+
+
+#define PACKETSIZE 516
+#define MAXDATA 512
+
+
+#define OP_RRQ 1
+#define OP_WRQ 2
+#define OP_DAT 3
+#define OP_ACK 4
+#define OP_ERR 5
+#define IS_RRQ(op) ((op)==OP_RRQ)
+#define IS_WRQ(op) ((op)==OP_WRQ)
+#define IS_DAT(op) ((op)==OP_DAT)
+#define IS_ACK(op) ((op)==OP_ACK)
+#define IS_ERR(op) ((op)==OP_ERR)
+
+
+#define ECODE_NONE  8
+#define ECODE_0     0
+#define IS_ECODE_0(ec)  ((ec) == ECODE_0)
+#define ESTRING_0   "Not defined, see error message(if any)."
+#define ECODE_1     1
+#define IS_ECODE_1(ec)  ((ec) == ECODE_1)
+#define ESTRING_1   "File not found."
+#define ECODE_2     2
+#define IS_ECODE_2(ec)  ((ec) == ECODE_2)
+#define ESTRING_2   "Access violation."
+#define ECODE_3     3
+#define IS_ECODE_3(ec)  ((ec) == ECODE_3)
+#define ESTRING_3   "Disk full or allocation exceeded."
+#define ECODE_4     4
+#define IS_ECODE_4(ec)  ((ec) == ECODE_4)
+#define ESTRING_4   "Illegal TFTP operation."
+#define ECODE_5     5
+#define IS_ECODE_5(ec)  ((ec) == ECODE_5)
+#define ESTRING_5   "Unknown transfer ID."
+#define ECODE_6     6
+#define IS_ECODE_6(ec)  ((ec) == ECODE_6)
+#define ECODE_7     7
+#define IS_ECODE_7(ec)  ((ec) == ECODE_7)
+#define ESTRING_7   "No such user."
+
+
+typedef unsigned short twobyte;
+
+typedef struct {
+	twobyte op;
+	char filename[PACKETSIZE];
+	char mode[PACKETSIZE];
+	char data[MAXDATA];
+	int data_l;
+	twobyte blnum;
+	twobyte errcode;
+	char errmes[PACKETSIZE];
+	int errmes_l;
+}packet;
