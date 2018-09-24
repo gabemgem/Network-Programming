8f4b3291b186c3310ce3b644fb9325bb4ebbc9e0
Skylar Sang <skylar.sang@gmail.com>
2018-09-23 21:47:30 -0400

    Updated Main File

--- HW01/assignment1.c
+++ HW01/assignment1.c
@@ -1,70 +1,67 @@
-#include "../unpv13e/lib/unp.h"
+//#include "unpv13e/lib/unp.h"
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <resolv.h>
+#include <strings.h>
+#include <netinet/in.h>
+#include "packet_handler.h"
+#include "file_handler.h"
+//#include "unp.h"
 
-int main(int argc, char **argv)
-{
-    int sockfd, connfd;
-    pid_t cpid;
-    struct sockaddr_in cliaddr, servaddr;
+#define BUFFSIZE 516
+#define sendrecvflag 0
+transaction t;
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servaddr.sin_port = htons(0);
+/*int file_buffer(transaction_t *transaction) {
+  if ((lseek(transaction->filedata, transaction->filepos, SEEK_SET)) == -1){
+    printf(stderr, "%s\n", strerror(errno));
+    return -1;
+  }
 
-    sockfd = Socket(AF_INET, SOCK_STREAM, 0);
-    if(Bind(sockfd, (SA *) &servaddr, sizeof(servaddr))==-1) {
-    	perror("Bind failed\n");
-    	exit(1);
+  if (int bytes_read = read(transaction->filedata, &transaction->filebuffer, 512) == -1) {
+    printf(stderr, "%s\n", strerror(errno));
+    return -1;
+  }
+  return bytes_read;
+}*/
+
+
+int main(int argc, char **argv) {
+  int sockfd, connfd;
+  pid_t cpid;
+  struct sockaddr_in cliaddr, servaddr;
+
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  servaddr.sin_port = 0;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  bind(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
+  printf("Waiting for connections");
+  //Listen(sockfd, LISTENQ);
+  while(1){
+    //connfd = Accept(listenfd, (SA*) &cliaddr, sizeof(cliaddr));
+    if (!fork){
+      //Area to handle client
+      char buf[BUFFSIZE];
+      char databuff[0];
+      int numbytes;
+      numbytes = recvfrom(sockfd, buf, BUFFSIZE, sendrecvflag, (struct sockaddr*) &cliaddr, sizeof(cliaddr));
+      printf("%d, %s\n", numbytes, buf);
+
+      //Process Packet for packet obj
+      //file_open_read(t.filename, t.filedata);
+
+
     }
-    printf("%d\n", servaddr.sin_port);
-    Listen(sockfd, LISTENQ);
-    while(1)
-    {
-        connfd = Accept(listenfd, (SA *) &cliaddr, sizeof(cliaddr));
-        if(connfd == -1) {
-        	perror("accept");
-        	continue;
-        }
-
-
-
-        cpid = Fork();
-        if (cpid == 0)
-        {
-            close(sockfd);
-            char buff[512];
-            int numBytes, ack_bytes;
-            header_t head;
-            packet_t packet;
-            packet_t ack;
-            char *filename = (char *) "recvfile.txt";
-
-            FILE *file;
-            bzero(buff, 512);
-            int done = 1;
-
-            while(!done)
-            {
-            	if((numBytes = recv(conn_fd, buff, 512, 0)) == -1) {
-            		perror("recv");
-            		fclose(file);
-            		close(conn_fd);
-            		exit(1);
-            	}
-
-
-            }
-        }
-    }
-
-
-
+  }
 }
+
 
 //Request to read or write file, request connection
 //If granted access, connection is opened
