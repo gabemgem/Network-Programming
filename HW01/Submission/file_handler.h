a7c1983b5304785e5b4d7b94ee2b81e39f7f0b48
gmaayan <gabemgem@gmail.com>
2018-09-24 22:24:47 -0400

    Moved deliverables into Submission folder

---
+++ HW01/Submission/file_handler.h
@@ -0,0 +1,7 @@
+
+
+int file_open_read(char *filename, int *filedata);
+int file_buffer_from_pos(transaction *transaction);
+int file_open_write(char *filename, int *filedata);
+int file_append_from_buffer(packet *packet, transaction *transaction);
+int file_close(int *filedata);
--- HW01/file_handler.h
+++
@@ -1,7 +0,0 @@
-
-
-int file_open_read(char *filename, int *filedata);
-int file_buffer_from_pos(transaction *transaction);
-int file_open_write(char *filename, int *filedata);
-int file_append_from_buffer(packet *packet, transaction *transaction);
-int file_close(int *filedata);
