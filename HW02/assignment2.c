6c9e4f7fbcd0fa47859fab22ce4b0aef474c7ce7
Skylar Sang <skylar.sang@gmail.com>
2018-10-14 14:56:51 -0400

    Added checking functions

--- HW02/assignment2.c
+++ HW02/assignment2.c
@@ -11,59 +11,140 @@
 #include <netinet/in.h>
 #include <signal.h>
 
+int MAX_CLIENTS = 5;
+int MAX_LINE = 1024;
+int ret[2];
+
 struct namestruct{
-  char n[100];
+  char n[100], hasname;
 };
 
+int checkWord(char* guess, char* word, int size, int* correct, int* placed){
+    //Assume input and word are the same length
+    int i, j; 
+    *correct = 0;
+    *placed = 0;
+
+    char temp[1024];
+    strcpy(temp, word);
+
+    for (i = 0; i < size; i++) {
+      for (j = 0; j < size; j++) {
+        if (guess[i] == temp[j]){
+          (*correct)++;
+
+          if (i == j) {
+            (*placed)++;
+          }
+
+          temp[j] = '\0';
+        }
+      }
+    }
+
+    return 1;
+}
+
+char checkName(char* buffer, struct namestruct* names){
+  int i;
+  for (i=0; i < MAX_CLIENTS; i++){
+    if (names[i].hasname){
+      if (strcmp(buffer, names[i].n) == 0){
+        return 0;
+      }
+    }
+  }
+
+  return 1;
+}
+
 int main(int argc, char ** argv){
-  int sockfd, client_set[5], i, sd, master, new_socket, value;
+  int sockfd, listenfd, connfd, maxfd, maxi, client_set[5], i, sd, value, num_clients, size, correct, placed;
+  unsigned int myPort;
   struct sockaddr_in cliaddr, servaddr;
   char secret_word[1024], buffer[1024];
   struct namestruct names[5];
-  fd_set rset;
+  fd_set rset, allset;
 
   bzero(&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   servaddr.sin_port = 0;
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if(bind(listenfd, (struct sockaddr*)&servaddr, (socklen_t)sizeof(servaddr)) == -1){
+		perror("Error binding");
+		exit(1);
+	}
+
+
+  struct sockaddr_in addr;
+  socklen_t addrlen = (socklen_t)sizeof(addr);
+  getsockname(sockfd, (struct sockaddr *)&addr, &addrlen);
+  myPort = ntohs(addr.sin_port);
+
+  if(listen(listenfd, 5)<0) {
+     perror("Error listening");
+     exit(1);
+  }
+
+  printf("Now Listening on Port %u\n", myPort);
 
   for (i = 0; i < 5; i++) {
     client_set[i] = 0;
     struct namestruct name;
-    strcpy(name.n, "");
-    names[i] = name;
+    name.hasname = 0;
   }
 
+  maxfd = listenfd;
+  maxi = -1;
+  num_clients = 0;
+
+  FD_ZERO(&allset);
+
   while(1){
-    FD_ZERO(&rset);
+    /*FD_ZERO(&rset);
     FD_SET(0, &rset);
     for (i = 0; i < 5; i++){
       sd = client_set[i];
       if(sd > 0) {
         FD_SET(sd, &rset);
       }
-    }
+    }*/
 
+    rset = allset;
+    sleep(1);
     printf("Waiting for selection\n");
-    select(FD_SETSIZE, &rset, NULL, NULL, NULL);
+    select(MAX_CLIENTS, &rset, NULL, NULL, NULL);
     printf("Select finished\n\n");
 
     //If new incoming connection
-    if (FD_ISSET(master, &rset)){
-      if ((new_socket = accept(master, (struct sockaddr*)&cliaddr, (socklen_t*)sizeof(&cliaddr)))<0){
-        perror("Accept");
+    if (FD_ISSET(listenfd, &rset) && num_clients < 5){ //add num_client barrier
+      
+      if ((connfd = accept(listenfd, (struct sockaddr*)&cliaddr, (socklen_t*) sizeof(&cliaddr)))<0){
+        perror("Error on accept");
         exit(EXIT_FAILURE);
       }
       printf("New Connection Has been Made");
-      char* user_message = "Welcome! Please Enter a Username";
       for (i = 0; i < 5; i++){
-        if(client_set[i] == 0){
-          client_set[i] = new_socket;
+        if(client_set[i] < 0){
+          client_set[i] = connfd;
           printf("Added to list of clients #%d\n", i);
+          strcpy(buffer, "Please Enter a Username");
+          send(connfd , buffer, MAX_LINE , 0 );
+          num_clients++;
           break;
         }
       }
+
+      FD_SET(connfd, &allset);
+    		if(connfd>maxfd)
+    			maxfd = connfd;
+    		if(num_clients-1>maxi)
+    			maxi = num_clients-1;
+    		//if(--nready <=0)
+    		//	continue;
     }
 
     for(i=0; i < 5; i++){
@@ -76,8 +157,28 @@ int main(int argc, char ** argv){
         }
         else {
           //PROCESS CLIENT INPUT
-          //Check if username is null -> check if name is not taken
-          //or process input as a guessed word
+            //Check if client already has -> check if name is not taken
+          if (!(names[i].hasname)){
+            if (checkName(buffer, names)){
+              names[i].hasname = 1;
+              strcpy(names[i].n, buffer);
+            }
+            else{
+              strcpy(buffer, "Username is already take, please choose a new one");
+              send(sd , buffer, MAX_LINE , 0 );
+            }
+            break;
+          }
+
+
+          else{
+            //or process input as a guessed word
+            read(sd, buffer, 1024);
+            checkWord(buffer, secret_word, size, &correct, &placed);
+            sprintf(buffer, "%s has guessed %s: %d letter(s) were correct and %d letter(s) were correctly placed", names[i].n, buffer, correct, placed);
+            
+          }
+
 
         }
       }
