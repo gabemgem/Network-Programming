f12df33ce38622ecfe4d87e4c3bcc345ef7689c4
gmaayan <gabemgem@gmail.com>
2018-10-15 01:26:15 -0400

    Fixed errors

--- HW02/assignment2.c
+++ HW02/assignment2.c
@@ -22,7 +22,13 @@ struct namestruct
     char n[100], hasname;
 };
 
-int checkWord(char *guess, char *word, int size, int *correct, int *placed)
+void removeNL(char *buffer)
+{
+    char NL = '\n';
+    buffer = strtok(buffer, &NL);
+}
+
+int checkWord(const char *guess, const char *word, int size, int *correct, int *placed)
 {
     //Assume input and word are the same length
     int i, j;
@@ -32,59 +38,64 @@ int checkWord(char *guess, char *word, int size, int *correct, int *placed)
     char temp[1024];
     strcpy(temp, word);
 
-    for (i = 0; i < size; i++)
+    for(int i = 0; i < size; i++)
     {
-        for (j = 0; j < size; j++)
+        for(int j = 0; j < size; j++)
         {
-            if (guess[i] == temp[j])
+            if(guess[i] == temp[j])
             {
                 (*correct)++;
-
-                if (i == j)
-                {
-                    (*placed)++;
-                }
-
-                temp[j] = '\0';
+                temp[j] = ' ';
+                break;
             }
         }
     }
 
-    return 1;
+    for(int k = 0; k < size; k++)
+    {
+        if(guess[k] == word[k])
+        {
+            (*placed)++;
+        }
+    }
+
+    return 0;
 }
 
 char checkName(char *buffer, struct namestruct *names)
 {
-    int i;
-    for (i = 0; i < MAX_CLIENTS; i++)
+    for (int i = 0; i < MAX_CLIENTS; i++)
     {
-        if (names[i].hasname)
+        if (names[i].hasname >= 0)
         {
             if (strcmp(buffer, names[i].n) == 0)
             {
-                return 0;
+                return 1;
             }
         }
     }
 
-    return 1;
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-    if(argc!=3) {
+    if(argc != 3)
+    {
         printf("Argument error: Please pass in dictionary file and max word length.\n");
         return 1;
     }
 
     int listenfd, connfd, maxfd, maxi, client_set[5], i, sd, value, num_clients, size, correct, placed;
     struct sockaddr_in cliaddr, servaddr;
-    char secret_word[1024], buffer[1024];
+    socklen_t clilen = sizeof(cliaddr);
+    char secret_word[MAX_LINE], buffer[MAX_LINE], guess[MAX_LINE], trash[MAX_LINE];
     struct namestruct names[5];
     fd_set rset, allset;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(listenfd<0) {
+    if(listenfd < 0)
+    {
         perror("Error opening socket");
         exit(1);
     }
@@ -118,11 +129,10 @@ int main(int argc, char **argv)
     for (i = 0; i < 5; i++)
     {
         client_set[i] = -1;
-        names[i].hasname = 0;
+        names[i].hasname = -1;
+        memset(names[i].n, ' ', sizeof(names[i].n));
     }
 
-    maxfd = listenfd;
-    maxi = -1;
     num_clients = 0;
 
     srand(time(NULL));
@@ -131,8 +141,9 @@ int main(int argc, char **argv)
         printf("Error setting up dict\n");
         exit(1);
     }
-    
+
     size = getWord(secret_word);
+    printf("Secret word: %s\n", secret_word);
 
 
     FD_ZERO(&allset);
@@ -143,42 +154,48 @@ int main(int argc, char **argv)
     {
 
         rset = allset;
-        sleep(1);
         printf("Waiting for selection\n");
         nready = select(FD_SETSIZE, &rset, NULL, NULL, NULL);
         printf("Select finished\n\n");
 
         //If new incoming connection
-        if (FD_ISSET(listenfd, &rset) && num_clients < 5)  //add num_client barrier
+        if (FD_ISSET(listenfd, &rset))  //add num_client barrier
         {
-
-            if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, (socklen_t *) sizeof(&cliaddr))) < 0)
-            {
-                perror("Error on accept");
-                exit(EXIT_FAILURE);
+            read(listenfd, buffer, 1024);
+            printf("Read from listen: %s\n", buffer);
+            if(num_clients>=5) {
+                connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+                memset(buffer, ' ', MAX_LINE);
+                strcpy(buffer, "Sorry we have the max number of players right now.\nPlease try again later.\n");
+                send(connfd, buffer, strlen(buffer), 0 );
+                close(connfd);
             }
-            printf("New Connection Has been Made");
-            for (i = 0; i < 5; i++)
-            {
-                if(client_set[i] < 0)
+            else {
+                if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen)) < 0)
                 {
-                    client_set[i] = connfd;
-                    printf("Added to list of clients #%d\n", i);
-                    strcpy(buffer, "Please Enter a Username");
-                    send(connfd, buffer, MAX_LINE, 0 );
-                    num_clients++;
-                    break;
+                    perror("Error on accept");
+                    exit(EXIT_FAILURE);
                 }
-            }
+                printf("New Connection Has been Made\n");
+                for (i = 0; i < 5; i++)
+                {
+                    if(client_set[i] < 0)
+                    {
+                        client_set[i] = connfd;
+                        printf("Added to list of clients #%d\n", i);
+                        memset(buffer, ' ', MAX_LINE);
+                        strcpy(buffer, "Please Enter a Username\n");
+                        send(connfd, buffer, strlen(buffer), 0 );
+                        num_clients++;
+                        break;
+                    }
+                }
 
-            FD_SET(connfd, &allset);
-            if(connfd > maxfd)
-                maxfd = connfd;
-            if(num_clients - 1 > maxi)
-                maxi = num_clients - 1;
-            nready--;
-            if(nready <=0)
-              continue;
+                FD_SET(connfd, &allset);
+                nready--;
+                if(nready <= 0)
+                    continue;
+            }
         }
 
         for(i = 0; i < 5; i++)
@@ -186,34 +203,43 @@ int main(int argc, char **argv)
             sd = client_set[i];
             if (FD_ISSET(sd, &rset))
             {
-                if ((value = read(sd, buffer, 1024)) == 0)
+                memset(buffer, 0, MAX_LINE);
+                value = read(sd, buffer, 1024);
+                if (value == 0)
                 {
                     //CLIENT DISCONNECTED
+                    printf("%s has disconnected\n", names[i].n);
                     close(sd);
                     client_set[i] = -1;
-                    names[i].hasname = 0;
+                    names[i].hasname = -1;
                     memset(names[i].n, 0, sizeof(names[i].n));
-                    //(names[i]).n = " ";
+                    num_clients--;
 
                 }
                 else
                 {
                     //PROCESS CLIENT INPUT
-                    read(sd, buffer, 1024);
+                    removeNL(buffer);
                     //Check if client already has -> check if name is not taken
-                    if (!(names[i].hasname))
+                    if (names[i].hasname < 0)
                     {
+                        printf("Putting a new username into slot #%d\n", i);
 
-                        if (checkName(buffer, names))
+                        if (checkName(buffer, names) == 0)
                         {
                             names[i].hasname = 1;
                             strcpy(names[i].n, buffer);
+                            memset(buffer, ' ', MAX_LINE);
+                            sprintf(buffer, "Welcome %s, the length of the secret word is %d\nPlease enter a guess:\n", names[i].n, size);
+                            printf("Everyone please welcome %s\n", names[i].n);
                         }
                         else
                         {
-                            strcpy(buffer, "Username is already take, please choose a new one");
-                            send(sd, buffer, MAX_LINE, 0 );
+                            memset(buffer, ' ', MAX_LINE);
+                            strcpy(buffer, "Username is already take, please choose a new one.\n");
+                            printf("What a dork their username was already taken.\n");
                         }
+                        send(sd, buffer, strlen(buffer), 0 );
                         break;
                     }
 
@@ -221,28 +247,38 @@ int main(int argc, char **argv)
                     else
                     {
                         //or process input as a guessed word
-                        
-                        checkWord(buffer, secret_word, size, &correct, &placed);
-                        if(placed == size) {
-                            sprintf(buffer, "%s has correctly guessed the word %s\n", names[i].n, secret_word);
+                        strcpy(guess, buffer);
+                        checkWord(guess, secret_word, size, &correct, &placed);
+                        memset(buffer, ' ', MAX_LINE);
+                        if(placed == size)
+                        {
+                            sprintf(buffer, "%s has correctly guessed the word %s\nYou are now being disconnected, goodbye.\n", names[i].n, secret_word);
                         }
-                        else {
-                            sprintf(buffer, "%s has guessed %s: %d letter(s) were correct and %d letter(s) were correctly placed", names[i].n, buffer, correct, placed);
+                        else
+                        {
+                            printf("%s has guessed %s: %d letter(s) were correct and %d letter(s) were correctly placed.\n", names[i].n, guess, correct, placed);
+                            sprintf(buffer, "%s has guessed %s: %d letter(s) were correct and %d letter(s) were correctly placed.\n", names[i].n, guess, correct, placed);
                         }
-                        for(i=0; i<5; ++i) {
-                            if(client_set[i]!=-1) {
-                                send(client_set[i], buffer, MAX_LINE, 0);
-                                if(placed == size) {
-                                    close(client_set[i]);
-                                    client_set[i] = -1;
-                                    names[i].hasname = 0;
-                                    memset(names[i].n, 0, sizeof(names[i].n));
-                                    //(names[i]).n = " ";
-                                }
+                        for(i = 0; i < 5; ++i)
+                        {
+                            if(client_set[i] != -1)
+                            {
+                                send(client_set[i], buffer, strlen(buffer), 0);
+                            }
+                            if(placed == size)
+                            {
+                                read(listenfd, trash, 1024);
+                                close(client_set[i]);
+                                client_set[i] = -1;
+                                names[i].hasname = -1;
+                                memset(names[i].n, 0, sizeof(names[i].n));
                             }
                         }
-                        if(placed == size) {
+                        if(placed == size)
+                        {
                             size = getWord(secret_word);
+                            printf("New secret word: %s\n", secret_word);
+                            num_clients=0;
                         }
 
 
